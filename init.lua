c9bde03efa0976159f697d7dd59c924d9b05770a
Kampferr <catherine.peters.dev@gmail.com>
2025-08-05 20:22:21 -0300

    fixed old LSP error

--- init.lua
+++ init.lua
@@ -9,3 +9,5 @@ vim.keymap.set("n", "<leader>so", ":source<CR>")
 vim.keymap.set("n", "<leader>q", ":quit<CR>")
 vim.opt.relativenumber = true
 vim.opt.number = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
